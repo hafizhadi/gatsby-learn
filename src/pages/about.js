2d1d2c0f9bbb607a5bd9ed079776616e5ef278eb
Hafizh Adi Prasetya <hafizh.adip@gmail.com>
2019-07-21 13:14:21 +0900

    FIrst commit

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Header from "../components/header"
+
+export default () => (
+    <div style={{ color: 'teal'}}>
+        <Header headerText="About Gatsby" />
+        <Header headerText="It's just as tiring as other javascript-based frameworks" />
+        <p>Such wow. Very react component.</p>
+    </div>
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,12 @@
 import React from "react"
+import { Link } from "gatsby"
+import Header from "../components/header"
 
-export default () => <div>Hello world!</div>
+export default () => (
+    <div style={{color: 'purple', fontSize: '15px' }}>
+        <Link to="/contact/">Contact</Link>
+        <h1>Hello again JavaScript you annoying language</h1>
+        <p>Goddamnit</p>
+        <img src="https://source.unsplash.com/random/400x200" alt=""/>
+    </div>
+)
