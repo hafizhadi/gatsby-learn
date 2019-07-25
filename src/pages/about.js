9f4e7ce64d32c07165f5546ebccb1822c244ae3d
Hafizh Adi Prasetya <hafizh.adip@gmail.com>
2019-07-25 12:06:03 +0900

    Finished part 3

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,9 @@
 import React from "react"
-import Header from "../components/header"
+import Layout from "../components/layout"
 
 export default () => (
-    <div style={{ color: 'teal'}}>
-        <Header headerText="About Gatsby" />
-        <Header headerText="It's just as tiring as other javascript-based frameworks" />
-        <p>Such wow. Very react component.</p>
-    </div>
+    <Layout>
+        <h1>About me</h1>
+        <p>I’m good enough, I’m smart enough, and gosh darn it, people like me!</p>
+    </Layout>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,10 @@
 import React from "react"
-import { Link } from "gatsby"
-import Header from "../components/header"
+import Layout from "../components/layout"
 
 export default () => (
-    <div style={{color: 'purple', fontSize: '15px' }}>
-        <Link to="/contact/">Contact</Link>
+    <Layout>
         <h1>Hello again JavaScript you annoying language</h1>
         <p>Goddamnit</p>
         <img src="https://source.unsplash.com/random/400x200" alt=""/>
-    </div>
+    </Layout>
 )
